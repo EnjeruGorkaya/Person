d3dce83eb8479adc39c2bb20abfb50ce2321ba5b
EnjeruGorkaya <tanya.meow13@gmail.com>
2017-12-13 13:20:53 +0300

    Initial commit

---
+++ src/main/java/ru/sbt/mipt/oop/PersonTest.java
@@ -0,0 +1,43 @@
+package main.java.ru.sbt.mipt.oop;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by 11007122 on 13.12.2017.
+ */
+public class PersonTest {
+    @Test
+    public void  createPersonTest () {
+        Person Mary = new Person(false, "Mary");
+        assertEquals(Mary.sayName(), "Mary");
+        Person John = new Person(true, "John");
+        assertEquals(John.sayName(), "John");
+    }
+
+    @Test
+    public void marryMaryJohn() throws Exception {
+        Person Mary = new Person(false, "Mary");
+        Person John = new Person(true, "John");
+        assertTrue(John.marry(Mary));
+    }
+
+    @Test
+    public void divorceMaryJohn() throws Exception {
+        Person Mary = new Person(false, "Mary");
+        Person John = new Person(true, "John");
+        John.marry(Mary);
+        assertTrue(John.divorce());
+    }
+
+    @Test
+    public void marryMaryJane() throws Exception {
+        Person Mary = new Person(false, "Mary");
+        Person Jane = new Person(false, "Jane");
+        assertFalse(Jane.marry(Mary));
+    }
+
+
+
+}
